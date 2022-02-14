2f4f46630a7350ebd43b906cb3104815cfd03d42
朱铭德 <zhumingde@ezxr.com>
2022-02-14 09:11:59 +0800

    feat($修复老照片): 修复老照片跑通

--- [6]3D物体识别_MediaPipe_Python/运行这个.py
+++ [6]3D物体识别_MediaPipe_Python/运行这个.py
@@ -7,10 +7,10 @@
 
 from utils.objectron_mediapipe import InputData, InitObjectron, ShowResult
 
-model_name = "Shoe"  # "Shoe" "Cup" "Chair" "Camera"
+model_name = "Shoe" # "Cup" "Chair" "Camera"
 # 初始化输入源, file支持数字（相机）以及视频文件路径，图片路径或文件夹路径
-input_data = InputData("test/imgs/" + model_name)  # input_data = InputData("test/Shoe.mp4")
-detect_3d_object = InitObjectron(object_name=model_name, static_mode=input_data.use_img_list)
+input_data = InputData("test/Shoe.mp4")  # input_data = InputData("test/imgs")
+detect_3d_object = InitObjectron(object_name="Shoe", static_mode=input_data.use_img_list)
 # 获取图像以及结果的generator
 run_pose_result = detect_3d_object.run_objectron(input_data.get_next_img())
 # 显示结果， ESC退出，图片模式按任意键继续
---
+++ [7]修复老照片_GFPGAN_Python/utils/gfpgan/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Thu Jan 13 15:48:05 2022
+__version__ = '0.2.4'
+__gitsha__ = 'd8bf32a'
+version_info = (0, 2, 4)
